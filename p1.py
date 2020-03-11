944e9544dcb4aea1de8e6d32e511f6265c8b5f3d
Francisca <raebeb@gmail.com>
2020-03-11 18:09:19 -0300

    final commit

--- p1.py
+++ p1.py
@@ -4,47 +4,47 @@
 #Extraer el máximo de la variable var1 de la región 4
 
 import json
+def open_json():
+  with open('data1.json') as f:
+    data = json.load(f)
 
-with open('data1.json') as f:
-  data = json.load(f)
+  data_string = json.dumps(data)
+  decoded = json.loads(data_string)
+  f.close()
+  return decoded
 
-data_string = json.dumps(data)
+def prom_var1():
+  i = 0
+  j = 0
+  k = 0
+  sum = 0
+  decoded = open_json()
+  for i in range(4):
+    for j in range(2):
+      for k in range(4):
+        sum = sum + int(decoded[i]["children"][j]["children"][k]["values"]["var1"])
 
-decoded = json.loads(data_string)
+  print('Promedio var1: {}'.format(sum/32))
 
-#print "Tenemos "+str(decoded["Fruteria"][1]["Verdura"][0]["Cantidad"])+" Lechugas."
+def suma_var2():
+  sum = 0
+  decoded = open_json()
+  for i in range(4):
+    sum = sum + int(decoded[0]["children"][1]["children"][i]["values"]["var2"]) 
+  print("suma var2 provincia 2: {}".format(sum))
 
-#promedioVar1 = int(decoded[0]["children"][0]["children"][0]["values"]["var1"])
-
-#prom = str(decoded[3]) --> Region
-
-#prom = str(decoded[0]["children"][2]) --> prov
-
-#prom = str(decoded[0]["children"][0]["children"][0]) --> ciuedad
-
-i = 0
-j = 0
-k = 0
-sum = 0
-for i in range(4):
+def max_var1():
+  max = 0
+  j = 0
+  k = 0
+  decoded = open_json()
   for j in range(2):
     for k in range(4):
-      sum = sum + int(decoded[i]["children"][j]["children"][k]["values"]["var1"])
-
-print("Promedio var1: ", sum/32)
-
-sum = 0
-for i in range(4):
-  sum = sum + int(decoded[0]["children"][1]["children"][i]["values"]["var2"]) 
-print("suma var2 provincia 2: ", sum)
-
-
-max = 0
-j = 0
-k = 0
-for j in range(2):
-  for k in range(4):
-    if(int(decoded[1]["children"][j]["children"][k]["values"]["var1"]) > max):
-      max = int(decoded[3]["children"][j]["children"][k]["values"]["var1"])
-print("El maximo de var1 en la region 4 es: ", max)
+      if(int(decoded[1]["children"][j]["children"][k]["values"]["var1"]) > max):
+        max = int(decoded[3]["children"][j]["children"][k]["values"]["var1"])
+  print("El maximo de var1 en la region 4 es: {}".format(max))
   
+if __name__ == '__main__':
+  prom_var1()
+  suma_var2()
+  max_var1()
--- p2.py
+++ p2.py
@@ -5,154 +5,166 @@ import json
 import time
 from datetime import datetime
 from datetime import time
-with open('data2.json') as f:
-  data = json.load(f)
+def open_json():
+  with open('data2.json') as f:
+    data = json.load(f)
 
-data_string = json.dumps(data)
+  data_string = json.dumps(data)
+  decoded = json.loads(data_string)
+  f.close()
+  return decoded
 
-decoded = json.loads(data_string)
+def promedio_zonas_espera():
+  cont = 0
+  decoded = open_json()
+  for i in decoded:
+    if (i["zone"] == "AE1" or i["zone"] == "AE2" or i["zone"] == "BE1" or i["zone"] == "BE2"):
+      if (cont == 0):
+        decodedTimein = datetime.fromisoformat(i["dt_in"])
+        decodedTimeOut = datetime.fromisoformat(i["dt_out"])
 
-cont = 0
-
-for i in decoded:
-  if (i["zone"] == "AE1" or i["zone"] == "AE2" or i["zone"] == "BE1" or i["zone"] == "BE2"):
-    if (cont == 0):
+        sum = decodedTimeOut - decodedTimein
+        cont += 1
+      
       decodedTimein = datetime.fromisoformat(i["dt_in"])
       decodedTimeOut = datetime.fromisoformat(i["dt_out"])
 
-      sum = decodedTimeOut - decodedTimein
+      totalTime = decodedTimeOut - decodedTimein
+      sum = sum + totalTime
       cont += 1
-    
-    decodedTimein = datetime.fromisoformat(i["dt_in"])
-    decodedTimeOut = datetime.fromisoformat(i["dt_out"])
 
-    totalTime = decodedTimeOut - decodedTimein
-    sum = sum + totalTime
-    cont += 1
+  print("El promedio de tiempo de espera en las areas AE1, AE2, BE1 y BE2 es: {}".format(sum/cont))
 
-print("El promedio de tiempo de espera en las areas AE1, AE2, BE1 y BE2 es: ", sum/cont)
-############################################################################################
-contW = 0
-total = 0
-for i in decoded:
-  if(i["zone"] == "AW2" or i["zone"] == "BW2"):
-    contW += 1
-    total += 1
-  else:
-    total += 1
-porc = (contW * 100) / total
+def porcentaje_espera():  
+  contW = 0
+  total = 0
+  decoded = open_json()
+  for i in decoded:
+    if(i["zone"] == "AW2" or i["zone"] == "BW2"):
+      contW += 1
+      total += 1
+    else:
+      total += 1
+  porc = (contW * 100) / total
 
-print("El porcentaje de ciclos de faena que incluyeron un area de trabajo de tipo dos es: %.2f"% porc, "%")
+  print("El porcentaje de ciclos de faena que incluyeron un area de trabajo de tipo dos es: %.2f"% porc, "%")
 
 ####################################################################################
 # que dia del mes se produjeron los mayores y menores tiempos de espera, en que estacion y a que camion corresponde
+def tiempos_espera():
+  cont = 0
+  decoded = open_json()
+  for i in decoded:
+    if (i["zone"] == "AE1" or i["zone"] == "AE2" or i["zone"] == "BE1" or i["zone"] == "BE2"):
+      decodedTimein = datetime.fromisoformat(i["dt_in"])
+      decodedTimeOut = datetime.fromisoformat(i["dt_out"])
+      totalTime = decodedTimeOut - decodedTimein
+      
+      if(cont == 0):
+        tMenor = totalTime
+        tMayor = totalTime
+        currentDateMayor = decodedTimein.strftime("%m/%d/%y")
+        truckNameMayor = i["asset"]
+        zoneMayor = i["zone"]
+        currentDateMenor = decodedTimein.strftime("%m/%d/%y")
+        truckNameMenor = i["asset"]
+        zoneMenor = i["zone"]
+        cont += 1
 
-cont = 0
-for i in decoded:
-  if (i["zone"] == "AE1" or i["zone"] == "AE2" or i["zone"] == "BE1" or i["zone"] == "BE2"):
-    decodedTimein = datetime.fromisoformat(i["dt_in"])
-    decodedTimeOut = datetime.fromisoformat(i["dt_out"])
-    totalTime = decodedTimeOut - decodedTimein
-    
-    if(cont == 0):
-      tMenor = totalTime
-      tMayor = totalTime
-      currentDateMayor = decodedTimein.strftime("%m/%d/%y")
-      truckNameMayor = i["asset"]
-      zoneMayor = i["zone"]
-      currentDateMenor = decodedTimein.strftime("%m/%d/%y")
-      truckNameMenor = i["asset"]
-      zoneMenor = i["zone"]
-      cont += 1
+      elif (totalTime > tMayor):
+        tMayor = totalTime
+        currentDateMayor = decodedTimein.strftime("%m/%d/%y")
+        truckNameMayor = i["asset"]
+        zoneMayor = i["zone"]
+        cont += 1
 
-    elif (totalTime > tMayor):
-      tMayor = totalTime
-      currentDateMayor = decodedTimein.strftime("%m/%d/%y")
-      truckNameMayor = i["asset"]
-      zoneMayor = i["zone"]
-      cont += 1
+      elif (totalTime < tMenor):
+        tMenor = totalTime
+        currentDateMenor = decodedTimein.strftime("%m/%d/%y")
+        truckNameMenor = i["asset"]
+        zoneMenor = i["zone"]
+        cont += 1
 
-    elif (totalTime < tMenor):
-      tMenor = totalTime
-      currentDateMenor = decodedTimein.strftime("%m/%d/%y")
-      truckNameMenor = i["asset"]
-      zoneMenor = i["zone"]
-      cont += 1
-
-print ("El mayor tiempo de espera fue de ", tMayor, " y se produjo el dia: ", currentDateMayor, "correspondiente al camion: ", truckNameMayor, "en la zona : ", zoneMayor)
-print ("El menor tiempo de espera fue de: ", tMenor, " y se produjo el dia: ", currentDateMenor, "correspondiente al camion: ", truckNameMenor, "en la zona: ", zoneMenor)
+  print ("El mayor tiempo de espera fue de {} y se produjo el dia: {} correspondiente al camion: {} en la zona : {}".format(tMayor, currentDateMayor, truckNameMayor, zoneMayor))
+  print ("El menor tiempo de espera fue de: {} y se produjo el dia: {} correspondiente al camion: {} en la zona: ".format(tMenor, currentDateMenor, truckNameMenor, zoneMenor))
 
 #En que zona se produce el mayor tiempo de espera
-cAE1 = 0
-cAE2 = 0
-cBE1 = 0
-cBE2 = 0
-zoneTime = []
-cont = 0
-for i in decoded:
-  if (i["zone"] == "AE1"):
-    decodedTimein = datetime.fromisoformat(i["dt_in"])
-    decodedTimeOut = datetime.fromisoformat(i["dt_out"])
-    totalTime = decodedTimeOut - decodedTimein
-    if (cAE1 == 0):
-      tAE1 = totalTime
-      cAE1+=1
-    else:
-      tAE1 = tAE1 + totalTime
-  
-  if (i["zone"] == "AE2"):
-    decodedTimein = datetime.fromisoformat(i["dt_in"])
-    decodedTimeOut = datetime.fromisoformat(i["dt_out"])
-    totalTime = decodedTimeOut - decodedTimein
-    if (cAE2 == 0):
-      tAE2 = totalTime
-      cAE2+=1
-    else:
-      tAE2 = tAE2 + totalTime
+def zona_mayor_espera():
+  cAE1 = 0
+  cAE2 = 0
+  cBE1 = 0
+  cBE2 = 0
+  zoneTime = []
+  decoded = open_json()
+  for i in decoded:
+    if (i["zone"] == "AE1"):
+      decodedTimein = datetime.fromisoformat(i["dt_in"])
+      decodedTimeOut = datetime.fromisoformat(i["dt_out"])
+      totalTime = decodedTimeOut - decodedTimein
+      if (cAE1 == 0):
+        tAE1 = totalTime
+        cAE1+=1
+      else:
+        tAE1 = tAE1 + totalTime
+    
+    if (i["zone"] == "AE2"):
+      decodedTimein = datetime.fromisoformat(i["dt_in"])
+      decodedTimeOut = datetime.fromisoformat(i["dt_out"])
+      totalTime = decodedTimeOut - decodedTimein
+      if (cAE2 == 0):
+        tAE2 = totalTime
+        cAE2+=1
+      else:
+        tAE2 = tAE2 + totalTime
 
-  if (i["zone"] == "BE1"):
-    decodedTimein = datetime.fromisoformat(i["dt_in"])
-    decodedTimeOut = datetime.fromisoformat(i["dt_out"])
-    totalTime = decodedTimeOut - decodedTimein
-    if (cBE1 == 0):
-      tBE1 = totalTime
-      cBE1+=1
-    else:
-      tBE1 = tBE1 + totalTime
+    if (i["zone"] == "BE1"):
+      decodedTimein = datetime.fromisoformat(i["dt_in"])
+      decodedTimeOut = datetime.fromisoformat(i["dt_out"])
+      totalTime = decodedTimeOut - decodedTimein
+      if (cBE1 == 0):
+        tBE1 = totalTime
+        cBE1+=1
+      else:
+        tBE1 = tBE1 + totalTime
 
-  if (i["zone"] == "BE2"):
-    decodedTimein = datetime.fromisoformat(i["dt_in"])
-    decodedTimeOut = datetime.fromisoformat(i["dt_out"])
-    totalTime = decodedTimeOut - decodedTimein
-    if (cBE2 == 0):
-      tBE2 = totalTime
-      cBE2+=1
-    else:
-      tBE2 = tBE2 + totalTime
+    if (i["zone"] == "BE2"):
+      decodedTimein = datetime.fromisoformat(i["dt_in"])
+      decodedTimeOut = datetime.fromisoformat(i["dt_out"])
+      totalTime = decodedTimeOut - decodedTimein
+      if (cBE2 == 0):
+        tBE2 = totalTime
+        cBE2+=1
+      else:
+        tBE2 = tBE2 + totalTime
 
-zoneTime.append(tAE1)
-zoneTime.append(tAE2)
-zoneTime.append(tBE1)
-zoneTime.append(tBE2)
+  zoneTime.append(tAE1)
+  zoneTime.append(tAE2)
+  zoneTime.append(tBE1)
+  zoneTime.append(tBE2)
 
-mayor = zoneTime[0]
-pos = 0
-for i in range(0,3):
-  print (zoneTime[i])
-  if (zoneTime[i] > mayor):
-    mayor = zoneTime[i]
-    pos = i
+  mayor = zoneTime[0]
+  pos = 0
+  for i in range(0,3):
+    if (zoneTime[i] > mayor):
+      mayor = zoneTime[i]
+      pos = i
 
-if (pos == 0):
-  eMayor = 'AE1'
+  if (pos == 0):
+    eMayor = 'AE1'
 
-if (pos == 1):
-  eMayor = 'AE2'
+  if (pos == 1):
+    eMayor = 'AE2'
 
-if (pos == 2):
-  eMayor = 'BE1'
+  if (pos == 2):
+    eMayor = 'BE1'
 
-if (pos == 3):
-  eMayor = 'BE2'
+  if (pos == 3):
+    eMayor = 'BE2'
 
-print("Durante el periodo el mayor tiempo de espera se produjo en la estacion",eMayor, "con un tiempo total de:",mayor)
+  print("Durante el periodo el mayor tiempo de espera se produjo en la estacion {} con un tiempo total acumulado de: {}".format(eMayor, mayor))
+
+if __name__ == '__main__':
+  promedio_zonas_espera()
+  porcentaje_espera()
+  tiempos_espera()
+  zona_mayor_espera()
